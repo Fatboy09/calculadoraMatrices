95d55462330f62e5bc651be52f34b61b6d3c44b9
Leiruz <jonysuri09@gmail.com>
2022-08-13 05:08:40 -0500

    se define el modelo y se crean los archivos fuente para trabajar con javafx

---
+++ src/com/leiruz/calculadora/controller/CalculadoraOverview.java
@@ -0,0 +1,13 @@
+package com.leiruz.calculadora.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CalculadoraOverview implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
---
+++ src/com/leiruz/calculadora/dto/maths/Matriz.java
@@ -0,0 +1,60 @@
+package com.leiruz.calculadora.dto.maths;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class Matriz {
+
+    private int rows;
+    private int columns;
+    private List<BigDecimal> matriz;
+
+    public Matriz(int rows, int columns, List<BigDecimal> matriz) {
+        this.rows = rows;
+        this.columns = columns;
+        this.matriz = matriz;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
+
+    public void setColumns(int columns) {
+        this.columns = columns;
+    }
+
+    public List<BigDecimal> getMatriz() {
+        return matriz;
+    }
+
+    public void setMatriz(List<BigDecimal> matriz) {
+        this.matriz = matriz;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Matriz matriz = (Matriz) o;
+
+        return new EqualsBuilder().append(getRows(), matriz.getRows()).append(getColumns(), matriz.getColumns()).append(getMatriz(), matriz.getMatriz()).isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37).append(getRows()).append(getColumns()).append(getMatriz()).toHashCode();
+    }
+}
