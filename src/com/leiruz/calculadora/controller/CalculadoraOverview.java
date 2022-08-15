d22d2a326e189152854373ca04245aae401a9896
Leiruz <jonysuri09@gmail.com>
2022-08-14 22:54:25 -0500

    se comienza el desarrollo de la vista de la interfaz gráfica

--- src/com/leiruz/calculadora/controller/CalculadoraOverview.java
+++ src/com/leiruz/calculadora/controller/CalculadoraOverview.java
@@ -1,13 +1,29 @@
 package com.leiruz.calculadora.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CalculadoraOverview implements Initializable {
+
+    private final ObservableList<String> strings = FXCollections.observableArrayList("filas", "columnas");
+
+    @FXML
+    private ComboBox<String> cb_MA;
+    @FXML
+    private ComboBox<String> cb_MB;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        cb_MA.setValue("filas");
+        cb_MB.setValue("filas");
+        cb_MA.setItems(strings);
+        cb_MB.setItems(strings);
 
     }
 }
